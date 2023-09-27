90c0bdb9869b9d62ba1b33cd1b25dd27b14fe288
Boubacar Kina <boubacarkina1@gmail.com>
2023-09-28 00:59:44 +0100

    lister les tashes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let tasks = document.getElementById("tasks");
 let form = document.getElementById("form");
 let msg = document.getElementById("msg");
 let categorie = document.getElementById("categorie");
@@ -16,5 +17,43 @@ let formValidation = () => {
         msg.innerHTML = "La tâche ne peut pas être vide";
     } else {
         msg.innerHTML = "";
+
+        acceptData();
     }
 }
+// local storage
+let data = [];
+let acceptData = () => {
+    data.push({
+        categorie: categorie.value,
+        titre: titre.value,
+        date: date.value,
+        textarea: textarea.value,
+        statut:statut.value,
+    })
+
+    localStorage.setItem("data", JSON.stringify(data));
+    
+    createTasks();
+}
+
+// Creation de nouvelles taches
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+      return (tasks.innerHTML += `
+      <div class="taches" id=${y}>
+            <p>${x.date}</p>
+            <p>${x.titre}</p>
+            <p>${x.categorie}</p>
+    
+            <p class="options">
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+            </p>
+          </div>
+      `);
+    });
+  
+    resetForm();
+  };
